1f8324c599d9b61535ee3262bc855a6a3f3c1ffa
Charles Opute Odili <chaluwa@gmail.com>
2021-02-01 13:50:03 +0100

    init codelab app

---
+++ src/app.js
@@ -0,0 +1,51 @@
+const digitPattern = /^\d+$/;
+const bvnPattern = /^\d{11}$/;
+let alreadyChecking = false;
+
+const verifyBVN = async (bvn) => {
+  try {
+    const response = await fetch('http://localhost:5000/verify/bvn', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ bvn })
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.warn(error);
+    return {
+      error: error.message
+    };
+  }
+};
+
+const handleInput = async (e) => {
+  const { target } = e;
+  const { value } = target;
+  if (alreadyChecking || !digitPattern.test(value) || !bvnPattern.test(value)) return;
+
+  alreadyChecking = true;
+  requestAnimationFrame(() => {
+    target.classList.remove('invalid', 'valid');
+    target.classList.add('busy');
+  });
+  const { verification, error } = await verifyBVN(value);
+
+  alreadyChecking = false;
+  requestAnimationFrame(() => {
+    target.classList.remove('busy');
+    target.removeAttribute('disabled');
+    let status = verification ? 'valid' : 'invalid';
+    status = error ? 'error' : status;
+    target.classList.add(status);
+  });
+};
+
+const startApp = () => {
+  const field = document.querySelector('[data-bvn-input]');
+  field.addEventListener('input', handleInput);
+};
+
+document.addEventListener('DOMContentLoaded', startApp);
